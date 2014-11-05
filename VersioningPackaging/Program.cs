bb3dfa29567d6b200bc109f65787084c28eb2f5c
Mikael Chudinov <mikael@chudinov.net>
2014-11-05 01:34:06 +0100

    Initial commit.

---
+++ VersioningPackaging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VersioningPackaging
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Versioning and Packaging!");
+        }
+    }
+}
