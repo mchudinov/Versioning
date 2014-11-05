e906a9eeaa116f5c9ef205ed50613af4d4137da8
Mikael Chudinov <mikael.chudinov@datametrix.no>
2014-11-05 11:39:22 +0100

    Clean AssemblyInfo file.

--- Versioning/Properties/AssemblyInfo.cs
+++ Versioning/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("VersioningPackaging")]
+[assembly: AssemblyTitle("Versioning")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -20,9 +17,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.0.1105.003")]
 [assembly: AssemblyFileVersion("1.0.1105.003")]
 
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
 
